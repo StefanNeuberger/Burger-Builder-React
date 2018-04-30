0168e15d5b5ae93971c40a79a8a5193faf374896
stefan neuberger <stefan@stefans-MBP.SpeedportEntry2090126250021>
2018-04-30 17:42:16 +0200

    added Burger component

---
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Aux from '../../../hoc/Aux';
+
+const burgerIngredient = (props) => {
+  return (
+    <Aux>
+
+    </Aux>
+  )
+};
+
+export default burgerIngredient;
